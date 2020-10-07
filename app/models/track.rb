61978405346312cd642f870710c31f557a01513f
Michael Gallup <Gallup93@icloud.com>
2020-10-07 12:57:52 -0600

    Update Track model to validate new attributes

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,7 +2,11 @@ class Track < ApplicationRecord
   validates :name, presence: true
   validates :album_name, presence: true
   validates :run_time, presence: true
-  validates :studio, presence: true
-  validates :live, presence: true
-  validates :cover, presence: true
+  validates :spotify_id, presence: true
+  validates :studio, inclusion: { in: [ true, false ] }
+  validates :live, inclusion: { in: [ true, false ] }
+  validates :cover, inclusion: { in: [ true, false ] }
+  validates :acoustic, inclusion: { in: [ true, false ] }
+  validates :base_version, inclusion: { in: [ true, false ] }
+  validates :unreleased, inclusion: { in: [true, false ] }
 end
